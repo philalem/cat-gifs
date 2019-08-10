416350dee8eebdbdd6cc60a7d5351c4006734db6
Abdullah <ansariabdullahmar@gmail.com>
2019-08-10 12:02:19 +0530

    initial commit

---
+++ src/component/gif.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CatGif extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+ 
+export default CatGif;
