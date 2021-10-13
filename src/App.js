3e678a33bdd964490f5ead130a5ad9498eec0e61
Phillip LeMaster <philalem21@gmail.com>
2021-10-13 13:42:19 -0400

    WHY WON'T IT WORK!?!?!?!?

--- src/App.js
+++ src/App.js
@@ -1,82 +1,87 @@
-import React from 'react';
-import CatGif from './component/CatGif';
-import {randomize} from './util/helper';
-import './App.css';
-const axios = require('axios');
-
+import React from "react";
+import CatGif from "./component/CatGif";
+import { randomize } from "./util/helper";
+import "./App.css";
+const axios = require("axios");
 
 class App extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			gifs : [],
-			// loading: true
-		}
-		this.fetchData = this.fetchData.bind(this)
-	}
-  
-	componentDidMount(){
-		this.fetchData()
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      // loading: true
+      // test comment
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
 
-	fetchData(){
-		console.log('button working')
-		console.log('loading')
+  componentDidMount() {
+    this.fetchData();
+  }
 
-		const setData = (gifs) => this.setState({ gifs });
-		setData([])
+  fetchData() {
+    console.log("button working");
+    console.log("loading");
 
-		const randomNumber = randomize(16, 1000)
+    const setData = (gifs) => this.setState({ gifs });
+    setData([]);
 
-		console.log("randomNumber", randomNumber)
+    const randomNumber = randomize(16, 1000);
 
-		// Make a request for a user with a given ID
-		axios.get('https://api.giphy.com/v1/gifs/search', {
-			params: {
-				api_key: 'm2FFtrSUnoPO1WYbu3TbXzuZFTpFAA4S',
-				q: 'cat',
-				limit: 10,
-				// offset: 85,
-				offset: randomNumber
-			}
-		})
+    console.log("randomNumber", randomNumber);
 
-		// axios.get('https://api.giphy.com/v1/gifs/random', {
-		// 	params: {
-		// 		api_key: '8T0Phxbr1meqMCol49pilsOxvvVyW78N',
-		// 		tag: 'burrito',
-		// 		rating: 'g',
-		// 		// random_id: 'e826c9fc5c929e0d6c6d423841a282aa'
-		// 	}
-		// })
-		
-		.then(function (response) {
-			// handle success
-			console.log(response.data.data);
-			if(response && response.data && response.data.data && response.data.data.length > 0 ){
-				setData(response.data.data);
-			}
-		})
-		.catch(function (error) {
-			// handle error
-			// console.log(error);
-		})
-		// console.log('1564')
-	}
-	render() { 
-		console.log("inrender:", this.state.gifs);
-		return (
-			<div>
+    // Make a request for a user with a given ID
+    axios
+      .get("https://api.giphy.com/v1/gifs/search", {
+        params: {
+          api_key: "m2FFtrSUnoPO1WYbu3TbXzuZFTpFAA4S",
+          q: "cat",
+          limit: 10,
+          // offset: 85,
+          offset: randomNumber,
+        },
+      })
 
-				{/* <CatGif data = {this.state.gifs} fetchData = {this.fetchData} /> */}
-				{
-					this.state.gifs && this.state.gifs.length > 0 ? 
-					<CatGif data = {this.state.gifs} fetchData = {this.fetchData} /> :
-					<p>Loading</p>
-				}
-			</div>
-		);
-	}
+      // axios.get('https://api.giphy.com/v1/gifs/random', {
+      // 	params: {
+      // 		api_key: '8T0Phxbr1meqMCol49pilsOxvvVyW78N',
+      // 		tag: 'burrito',
+      // 		rating: 'g',
+      // 		// random_id: 'e826c9fc5c929e0d6c6d423841a282aa'
+      // 	}
+      // })
+
+      .then(function (response) {
+        // handle success
+        console.log(response.data.data);
+        if (
+          response &&
+          response.data &&
+          response.data.data &&
+          response.data.data.length > 0
+        ) {
+          setData(response.data.data);
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        // console.log(error);
+      });
+    // console.log('1564')
+  }
+  render() {
+    console.log("inrender:", this.state.gifs);
+    return (
+      <div>
+        {/* <CatGif data = {this.state.gifs} fetchData = {this.fetchData} /> */}
+        {this.state.gifs && this.state.gifs.length > 0 ? (
+          <CatGif data={this.state.gifs} fetchData={this.fetchData} />
+        ) : (
+          <p>Loading</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
